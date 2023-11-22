3322aa435d792d432940c89eae0ffc852a4187f4
FabrizzioDLCR <151418678+FabrizzioDLCR@users.noreply.github.com>
2023-11-22 13:17:23 -0500

    Delete ICategoriaInsumoRepository.java

--- ICategoriaInsumoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.mac.repository;
-
-import com.project.mac.model.entity.CategoriaInsumo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICategoriaInsumoRepository extends JpaRepository<CategoriaInsumo, Integer> {
-}
